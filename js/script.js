ec584eacb315face481c0c9093f4c965c302753a
Anturia Hassani <antu.ria-75@live.fr>
2024-02-27 21:19:17 +0100

    Modification de la gestion des calculs entrés dans la calculatrice (ajout d'un switch pour gérer les différents cas). Gestion des calculs en chaîne, des nombres négatifs et ajout d'un bouton supprimer.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let button = document.getElementsByTagName("button");
-    let egal = document.getElementById("egal");
-    let res = document.getElementById("res");   
+    let res = document.getElementById("res");
+    const regex = /(-?\d+(\.\d+)?|[+\-*/])/g;
+    let isResultDisplayed = false;
+
 
     for (let i = 0; i < button.length; i++) {
         button[i].addEventListener(
-            "mouseover", 
+            "mouseover",
             function (event) {
                 event.target.style.backgroundColor = "grey";
             }
         );
 
         button[i].addEventListener(
-            "mouseout", 
+            "mouseout",
             function (event) {
                 event.target.style.backgroundColor = "";
             }
@@ -21,71 +23,97 @@ document.addEventListener("DOMContentLoaded", function() {
         button[i].addEventListener(
             "click",
             function (event) {
+                let expression;
                 event.target.style.backgroundColor = "white";
-                res.textContent += button[i].textContent.trim();
-                if (button[i].textContent == egal.textContent){
-                    let leresultat = Array.from(res.textContent.trim());
-                    while (leresultat.length > 1){
-                        leresultat = resultat(leresultat);
+                if (!isResultDisplayed && button[i].value != "suppr.") {
+                    res.innerHTML += button[i].value;
+                    if (button[i].value == "=") {
+                        expression = res.textContent.trim().match(regex);
+                        resultat = calcul(expression);
+                        res.innerHTML += resultat;
+                        isResultDisplayed = true;
                     }
-                    res.textContent += Number(leresultat);
-                }  
+                } else if (!isResultDisplayed && button[i].value == "suppr."){
+                    expression = res.textContent.trim().match(regex);
+                    expression.pop();
+                    res.innerHTML = expression;
+                } else if (isResultDisplayed && button[i].value == "suppr.") {
+                    res.innerHTML = "";
+                    isResultDisplayed = false;
+                }
             }
         );
+
     }
+
 });
 
-function multiplier (num1,num2){
+function multiplier(num1, num2) {
     return num1 * num2;
 }
 
-function add (num1,num2){
-    return num1 + num2;
+function add(num1, num2) {
+    return parseFloat(num1) + parseFloat(num2);
 }
 
-function soustraire (num1,num2){
+function soustraire(num1, num2) {
     return num1 - num2;
 }
 
-function diviser (num1,num2){
-    return num1/num2;
+function diviser(num1, num2) {
+    return num1 / num2;
 }
 
-function resultat (leresultat)
-{
-    let tab = leresultat;
+function calcul(tab) {
     let tab2 = [];
-    
-    for (let i=0;i<tab.length;i++){
 
-        if (tab[i+1]=='/'){
-            tab2.push(diviser(tab[i],tab[i+2]));
-            tab2.push(tab[i+3]);
-        }
-
-        if (tab[i+1]=='*'){
-           tab2.push(multiplier(tab[i],tab[i+2]));
-           tab2.push(tab[i+3]);
-        }
-
-        if (tab[i+1]=='-'){
-            if(tab2.length == 0) {
-                tab2.push(soustraire(tab[i],tab[i+2]));
-            }            
-        }
-
-        if (tab[i+1]=='+'){
-            if(tab2.length === 0) {
-            tab2.push(add(tab[i],tab[i+2]));
-        }
+    switch (tab[1]) {
+        case '+':
+            if (tab[3] != '*' && tab[3] != '/') {
+                tab2.push(add(tab[0], tab[2]));
+                tab = tab.slice(3);
+                tab2 = tab2.concat(tab);
+            } else {
+                tab2 = tab2.concat(tab.slice(0, 2));
+                tab2 = tab2.concat(calcul(tab.slice(2)));
+            }
+            break;
+        case '-':
+            if (tab[3] != '*' && tab[3] != '/') {
+                tab2.push(soustraire(tab[0], tab[2]));
+                tab = tab.slice(3);
+                tab2 = tab2.concat(tab);
+            } else {
+                tab2 = tab2.concat(tab.slice(0, 2));
+                tab2 = tab2.concat(calcul(tab.slice(2)));
+            }
+            break;
+        case '*':
+            tab2.push(multiplier(tab[0], tab[2]));
+            tab = tab.slice(3);
+            tab2 = tab2.concat(tab);
+            break;
+        case '/':
+            tab2.push(diviser(tab[0], tab[2]));
+            tab = tab.slice(3);
+            tab2 = tab2.concat(tab);
+            break;
+        default:
+            if (tab[3] != '*' && tab[3] != '/') {
+                tab2.push(soustraire(tab[0], Math.abs(tab[1])));
+                tab = tab.slice(2);
+                tab2 = tab2.concat(tab);
+            } else {
+                tab2 = tab2.concat(tab.slice(0, 1));
+                tab2 = tab2.concat(calcul(tab.slice(1)));
+            }                        
     }
-    }
 
-    if(tab2[tab2.length-1]=='='){
-        tab2.pop();
+    if (tab2.length > 1) {
+        return calcul(tab2);
     }
-    
-    return tab2;
-
+    else {
+        return tab2[0];
+    }
 
 }
