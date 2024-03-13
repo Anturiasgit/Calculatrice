e2325af047b4225cb8c5ea4543e1ed9c63fed923
Anturia Hassani <antu.ria-75@live.fr>
2024-03-13 12:05:54 +0100

    Optimisation de la lisibilité du code et ajout des commentaires

--- js/script.js
+++ js/script.js
@@ -1,59 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {     // "DOMContentLoaded" nécessaire car sans lui, les fonctions sont appelés avant que le contenu intégral de la page ne termine soit chargé
     let button = document.getElementsByTagName("button");
     let res = document.getElementById("res");
     const regex = /(-?\d+(\.\d+)?|[+\-*/])/g;
     let isResultDisplayed = false;
+    let expression;     /* pour l'expression du calcul */
+
+
+    /* "button" est un tableau contenant tous les boutons. La constante "regex" nous permet de transformer 
+        l'expression du calcul en un tableau contenant un nombre ou un opérande dans chaque case. 
+        le booléen "isResultDisplayed" est initié à "false" car au départ aucun résultat n'est affiché. 
+        Il sert notament de repère pour les traitements de clique sur les boutons de suppression. */
 
 
     for (let i = 0; i < button.length; i++) {
-        button[i].addEventListener(
-            "mouseover",
-            function (event) {
-                event.target.style.backgroundColor = "lightblue";
-            }
-        );
 
         button[i].addEventListener(
-            "mouseout",
-            function (event) {
-                event.target.style.backgroundColor = "";
-            }
-        );
-
-        button[i].addEventListener(
-            "click",
-            function (event) {
-                let expression;
-
-                event.target.style.backgroundColor = "white";
-                if (!isResultDisplayed && button[i].value != "effacer" && button[i].value != "effacer_tout") {
-                    res.innerHTML += button[i].value;
-                    if (button[i].value == "=") {
-                        expression = res.textContent.trim().match(regex);
-                        resultat = calcul(expression);
-                        res.innerHTML += resultat;
-                        isResultDisplayed = true;
-                        
-                    }
-                } else if (!isResultDisplayed && button[i].value == "effacer") {
-                    res.innerHTML = res.textContent.trim().slice(0, -1);
-                } else if (!isResultDisplayed && button[i].value == "effacer_tout") {
-                    res.innerHTML = "";
-                } else if (isResultDisplayed && button[i].value == "effacer_tout") {
-                    res.innerHTML = "";
-                    isResultDisplayed = false;
-                }
+            "click",            /*Gestion des évènements de clique*/
+            function () {
+                if (!isResultDisplayed){
+                    if(button[i].value != "effacer" && button[i].value != "effacer_tout"){
+                       res.innerHTML += button[i].value;        /* Transcrit la valeur du bouton dans la div du résultat */
+                       if (button[i].value == "=") {
+                           expression = res.textContent.trim().match(regex);        /* "trim" pour enlever les espaces inutiles et match(regex) pour transformer la chaîne de caractère "expression" en un tableau */
+                           resultat = calcul(expression);       /* "resultat" est du type "number" */
+                           res.innerHTML += resultat;          
+                           isResultDisplayed = true;
+                       }
+                   } else if (button[i].value == "effacer") {
+                       res.innerHTML = res.textContent.trim().slice(0, -1);     /* "slice" renvoie la chaîne de caractère privé de son dernier élément */
+                   } else if (button[i].value == "effacer_tout"){
+                       res.innerHTML = "";
+                   }
+               } else if (button[i].value == "effacer_tout"){
+                   res.innerHTML = "";
+                   isResultDisplayed = false;
+               }
             });
     }
-
 });
+
+
+/* Fonctions qui traitent les calculs :
+avec une fonction principale "calcul" qui reçoit l'expression 
+et fait appel aux fonctions concernées selon le type d'opérande 
+rencontrée durant le traitement de l'expression. */
 
 function multiplier(num1, num2) {
     return num1 * num2;
 }
 
 function add(num1, num2) {
-    return parseFloat(num1) + parseFloat(num2);
+    return parseFloat(num1) + parseFloat(num2);     // parseFloat nécessaire car évite au caractère "+" de traiter "num1" et "num2" comme deux chaînes de caractères à concaténer mais bien comme des nombres à additionner 
 }
 
 function soustraire(num1, num2) {
@@ -67,37 +64,37 @@ function diviser(num1, num2) {
 function calcul(tab) {
     let tab2 = [];
 
-    if(tab.length == 1) {
+    if (tab.length == 1) {
         return tab[0];
     }
 
-    switch (tab[1]) {
+    switch (tab[1]) {       // "tab[0] étant un nombre, on se focalise sur la valeur de tab[1]" 
         case '+':
-            if (tab[3] != '*' && tab[3] != '/') {
-                tab2.push(add(tab[0], tab[2]));
-                tab = tab.slice(3);
-                tab2 = tab2.concat(tab);
+            if (tab[3] != '*' && tab[3] != '/') {       // Gestion des priorités de calcul en vérifiant si l'opérande qui suit est prioritaire ou non 
+                tab2.push(add(tab[0], tab[2]));     // Appel de la fonction d'addition sur les opérateurs concernés et ajout du résultat dans un nouveau tableau : "tab2" 
+                tab = tab.slice(3);     // Mise à jour du tableau inital (sans l'expression qui vient d'être calculée) 
+                tab2 = tab2.concat(tab);        // Concaténation du nouveau tableau avec le tableau initial afin de poursuivre le traitement du résultat 
             } else {
                 tab2 = tab2.concat(tab.slice(0, 2));
-                tab2 = tab2.concat(calcul(tab.slice(2)));
+                tab2 = tab2.concat(calcul(tab.slice(2)));       // Si l'opérande qui suit est prioritaire sur l'addition, on prend les 2 premiers éléments du tableau initial pour les ajouter au nouveau tableau, et on laisse le reste être traité de nouveau par la fonction 
             }
             break;
-        case '-':
-            if (tab[3] != '*' && tab[3] != '/') {
-                tab2.push(soustraire(tab[0], tab[2]));
-                tab = tab.slice(3);
-                tab2 = tab2.concat(tab);
+        case '-':       // Le procédé est le même que pour l'addition 
+            if (tab[3] != '*' && tab[3] != '/') {       
+                tab2.push(soustraire(tab[0], tab[2]));      
+                tab = tab.slice(3);     
+                tab2 = tab2.concat(tab);        
             } else {
                 tab2 = tab2.concat(tab.slice(0, 2));
-                tab2 = tab2.concat(calcul(tab.slice(2)));
+                tab2 = tab2.concat(calcul(tab.slice(2)));      
             }
             break;
-        case '*':
-            tab2.push(multiplier(tab[0], tab[2]));
-            tab = tab.slice(3);
+        case '*':       // Procédé similaire à l'addition et à la soustraction, sans vérification de la priorité car la multiplication est prioritaire */
+            tab2.push(multiplier(tab[0], tab[2]));      
+            tab = tab.slice(3);   
             tab2 = tab2.concat(tab);
             break;
-        case '/':
+        case '/':       // Procédé similaire à la multiplication, avec vérification de la division par 0 
             if (tab[2] == 0) {
                 tab2[0] = "Division par 0 impossible";
             } else {
@@ -107,21 +104,15 @@ function calcul(tab) {
             }
             break;
         default:
-            if (tab[3] != '*' && tab[3] != '/') {
-                tab2.push(soustraire(tab[0], Math.abs(tab[1])));
-                tab = tab.slice(2);
-                tab2 = tab2.concat(tab);
-            } else {
-                tab2 = tab2.concat(tab.slice(0, 1));
-                tab2 = tab2.concat(calcul(tab.slice(1)));
-            }
+        // Aucune action nécessaire ici
+        break;
     }
 
-    if (tab2.length > 1) {
+    if (tab2.length > 1) {      // Si le tableau nouvellement formé contient plus d'une case, c'est que les calculs ne sont pas terminés, alors on rappelle la fonction sur ce tableau
         return calcul(tab2);
     }
     else {
-        return tab2[0];
+        return tab2[0];     // Dans le cas où le tableau ne contient qu'une seule case, son élement (un "number") peut être retourné comme résultat final     
     }
 
 }
